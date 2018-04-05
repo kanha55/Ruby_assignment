e273fd7220cee832bec05357db1fc5ae5e9fb91d
kanha55 <kanha.dubey9@gmail.com>
2018-04-05 18:39:02 +0530

    inverted number program

--- inverted?_number.rb
+++ inverted?_number.rb
@@ -4,11 +4,8 @@ def inverted?(input_number)
   copy_input_number = input_number
   digit_of_number = 0
   reverse_number = 0
-
   while copy_input_number > 0
-   
     digit_of_number = copy_input_number % 10
-      
       if(digit_of_number == 6)
         reverse_number = reverse_number*10 + 9
       elsif(digit_of_number == 9)
@@ -20,10 +17,8 @@ def inverted?(input_number)
       elsif(digit_of_number == 8)
         reverse_number = reverse_number*10 + 8
       end 
-
      copy_input_number = copy_input_number / 10
   end
-
   if(input_number == reverse_number)
   return true
   end   
