7f75d4909290b80c3709e95ef0243907d50ef13e
kanha55 <kanha.dubey9@gmail.com>
2018-04-05 18:40:23 +0530

    inverted number program

--- inverted?_number.rb
+++ inverted?_number.rb
@@ -4,21 +4,23 @@ def inverted?(input_number)
   copy_input_number = input_number
   digit_of_number = 0
   reverse_number = 0
-  while copy_input_number > 0
-    digit_of_number = copy_input_number % 10
-      if(digit_of_number == 6)
-        reverse_number = reverse_number*10 + 9
-      elsif(digit_of_number == 9)
-        reverse_number = reverse_number*10 + 6
-      elsif(digit_of_number == 0)
-        reverse_number = reverse_number*10 + 0
-      elsif(digit_of_number == 1)
-        reverse_number = reverse_number*10 + 1
-      elsif(digit_of_number == 8)
-        reverse_number = reverse_number*10 + 8
-      end 
-     copy_input_number = copy_input_number / 10
-  end
+
+    while copy_input_number > 0
+      digit_of_number = copy_input_number % 10
+        if(digit_of_number == 6)
+          reverse_number = reverse_number*10 + 9
+        elsif(digit_of_number == 9)
+          reverse_number = reverse_number*10 + 6
+        elsif(digit_of_number == 0)
+          reverse_number = reverse_number*10 + 0
+        elsif(digit_of_number == 1)
+          reverse_number = reverse_number*10 + 1
+        elsif(digit_of_number == 8)
+          reverse_number = reverse_number*10 + 8
+        end 
+       copy_input_number = copy_input_number / 10
+    end
+    
   if(input_number == reverse_number)
   return true
   end   
